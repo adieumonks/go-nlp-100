bf0168c39aaea303a616157230a9fc27ca071485
sudoken <44742486+adieumonks@users.noreply.github.com>
2023-11-25 00:15:26 +0900

    add ex07

---
+++ ex07/go.mod
@@ -0,0 +1,3 @@
+module github.com/adieumonks/go-nlp-100/ex07
+
+go 1.21.3
---
+++ ex07/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func template(x int, y string, z float64) string {
+	return fmt.Sprintf("%d時の%sは%.1f", x, y, z)
+}
+
+func main() {
+	x := 12
+	y := "気温"
+	z := 22.4
+	fmt.Println(template(x, y, z))
+
+}
